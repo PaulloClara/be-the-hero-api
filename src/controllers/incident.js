7a7f6639529459418cd867a1e4224b6d9937bccf
paulloclara <paulloclara@gmail.com>
2020-03-26 15:29:15 -0300

    :rocket: Config pagination

--- src/controllers/incident.js
+++ src/controllers/incident.js
@@ -2,7 +2,24 @@ const database = require("../database");
 
 module.exports = {
   async index(request, response) {
-    const incidents = await database("incidents").select("*");
+    const { page = 1 } = request.query;
+
+    const [count] = await database("incidents").count();
+
+    const incidents = await database("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
+
+    response.header("X-Total-Count", count["count(*)"]);
 
     return response.status(200).json(incidents);
   },
