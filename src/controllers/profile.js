31df1f0e53caedc452bb964e24caccf16a3422bd
paulloclara <paulloclara@gmail.com>
2020-03-26 15:05:36 -0300

    :rocket: Config list of incidents according to the ong id

---
+++ src/controllers/profile.js
@@ -0,0 +1,13 @@
+const database = require("../database");
+
+module.exports = {
+  async index(request, response) {
+    const ong_id = request.headers.authorization;
+
+    const incidents = await database("incidents")
+      .where("ong_id", ong_id)
+      .select("*");
+
+    return response.status(200).json(incidents);
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const routes = require("express").Router();
 
 const OngController = require("./controllers/ong");
+const ProfileController = require("./controllers/profile");
 const IncidentController = require("./controllers/incident");
 
 routes.get("/ongs", OngController.index);
@@ -9,5 +10,7 @@ routes.post("/ongs", OngController.store);
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.store);
 routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.get("/profile", ProfileController.index);
 
 module.exports = routes;
