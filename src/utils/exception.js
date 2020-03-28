da0010cd05330948192703c793cd1485944d1569
paulloclara <paulloclara@gmail.com>
2020-03-28 10:53:24 -0300

    :rocket: Add bad request response

--- src/utils/exception.js
+++ src/utils/exception.js
@@ -3,6 +3,10 @@ module.exports = {
     this.response.status(code).json({ error: message });
   },
 
+  badRequest(message) {
+    this.newError(message, 400);
+  },
+
   notFound(message) {
     this.newError(message, 404);
   },
