df6999e12f8ceac483a88a1e7099aa3a2e1af541
paulloclara <paulloclara@gmail.com>
2020-03-28 11:48:13 -0300

    :rocket: Config jsonwebtoken

---
+++ src/utils/jwt.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  sign(id) {
+    return jwt.sign({ id }, process.env.JWT_PRIVATE_KEY, {
+      expiresIn: Number(process.env.JWT_VALIDITY)
+    });
+  },
+
+  verify(token) {
+    return jwt.verify(token, process.env.JWT_PRIVATE_KEY, (error, result) =>
+      error ? null : result
+    );
+  }
+};
