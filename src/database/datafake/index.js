9cae62ff6a516a3df9fe8c4524894413f0715fb5
paulloclara <paulloclara@gmail.com>
2020-03-29 12:39:11 -0300

    :wrench: Set up false data for testing

---
+++ src/database/datafake/index.js
@@ -0,0 +1,40 @@
+const request = require("request");
+const { resolve } = require("path");
+const { readFileSync: readFile } = require("fs");
+
+const tokens = [];
+
+function sendRequest(route, data, token) {
+  return request(
+    {
+      url: `http://localhost:3000/${route}`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(data)
+    },
+    (error, { body }) => {
+      body = JSON.parse(body);
+      tokens.push(body.token);
+    }
+  );
+}
+
+setTimeout(() => {
+  const min = 0;
+  const max = tokens.length;
+
+  incidents.forEach(async incident => {
+    const token = tokens[Math.floor(Math.random() * (max - min) + min)];
+    await sendRequest("incidents", incident, token);
+  });
+}, 20000);
+
+const ongs = JSON.parse(readFile(resolve(__dirname, "ongs.json")));
+const incidents = JSON.parse(readFile(resolve(__dirname, "incidents.json")));
+
+ongs.forEach(async ong => {
+  await sendRequest("ongs", ong);
+});
