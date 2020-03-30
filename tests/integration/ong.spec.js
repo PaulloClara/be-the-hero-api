2c9bc96c7d919f4ab21b7aaf537d31afc9434f97
paulloclara <paulloclara@gmail.com>
2020-03-30 01:52:18 -0300

    :wrench: Config jest

---
+++ tests/integration/ong.spec.js
@@ -0,0 +1,5 @@
+describe("Testing Jest", () => {
+  it("Calculating 2+2", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ tests/unit/bcrypt.spec.js
@@ -0,0 +1,5 @@
+describe("Testing Jest", () => {
+  it("Calculating 1+1", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
