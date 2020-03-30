e3688fcc38ba088cc10bb22061886792944e4a79
paulloclara <paulloclara@gmail.com>
2020-03-29 23:28:53 -0300

    :rocket: Config celebrate error

--- src/server.js
+++ src/server.js
@@ -7,9 +7,10 @@ server.use(require("cors")());
 server.use(express.json());
 server.use(require("morgan")("tiny"));
 server.use(require("./routes"));
+server.use(require("celebrate").errors());
 
 const PORT = 3000;
 
-server.listen(PORT, _ => {
+server.listen(PORT, () => {
   console.log(`\n\tRunning on localhost:${PORT}\n`);
 });
